eed6fe0149799889f0318ed619d132295e973c9d
ledbutter <svengrosen@gmail.com>
2014-11-07 15:33:42 -0600

    Updated content

--- PageContent.js
+++ PageContent.js
@@ -23,32 +23,31 @@ var PageContent = function (pageNumber) {
         case 3:
             this.title = "Client Delight";
 						this.description = "This measures the Consultant's ability to delight the client and produce excellent deliverables. This measurement is obtained primarily from client formal/informal feedback, client e-mails, client meetings, OTACE documentation, or feedback from UDM, AE or Project Managers";
-            this.contents[0] = new MainPoint("Main Point 1", ["Sub Point 1.1", "Sub Point 1.2"]);
-            this.contents[1] = new MainPoint("Main Point 2", ["Sub Point 2.1", "Sub Point 2.2", "Sub Point 2.3"]);
+            this.contents[0] = new MainPoint("Thomson Reuters", ["OTACE", "Extensions"]);
             break;
 		case 4:
             this.title = "Productivity";
 						this.description = "This reflects the total effort put forth by the consultant. The goal for all Consultants is to remain fully utilized with their contributions to the client as well as to the unit. URVE (Utilization Rate Vacation Excluded) is just one dial of measurement we use in this category: Utilization Rate. It is simply the ratio of hours billed to available hours (less PTO).";
-            this.contents[0] = new MainPoint("Main Point 1", ["Sub Point 1.1", "Sub Point 1.2"]);
-            this.contents[1] = new MainPoint("Main Point 2", ["Sub Point 2.1", "Sub Point 2.2", "Sub Point 2.3"]);
+            this.contents[0] = new MainPoint("Utilization", []);
+            this.contents[1] = new MainPoint("Client Committment", []);
             break;
 		case 5:
             this.title = "Business Development";
 						this.description = "Consistently contributing to our sales pipeline results by powerfully leveraging others (internal / external) and allowing oneself to be fully leveraged by others in sales pursuits so Sogeti wins.   Demonstrating excellence through individual accomplishments, team accomplishments AND partner relationship growth. Active participation in the Collaborative Selling and Selling While Delivering processes.";
-            this.contents[0] = new MainPoint("Main Point 1", ["Sub Point 1.1", "Sub Point 1.2"]);
-            this.contents[1] = new MainPoint("Main Point 2", ["Sub Point 2.1", "Sub Point 2.2", "Sub Point 2.3"]);
+            this.contents[0] = new MainPoint("TR Relationships", []);
+            this.contents[1] = new MainPoint("AE Relationship", ["Aborted Proposal", "Leads", "Client Hires"]);
             break;
 		case 6:
             this.title = "Delivery Excellence";
 						this.description = "As a trusted advisor, consistently delivering exemplary service to our clients by executing the Sogeti Delivery model/best practices (differentiators, tools, processes, methods), implementing an appropriate level of risk and delivering on financial commitments to achieve profitability. Incorporates all project management & control best practices.";
-            this.contents[0] = new MainPoint("Main Point 1", ["Sub Point 1.1", "Sub Point 1.2"]);
+            this.contents[0] = new MainPoint("Sogeti Security Certification", []);
             this.contents[1] = new MainPoint("Main Point 2", ["Sub Point 2.1", "Sub Point 2.2", "Sub Point 2.3"]);
             break;
 		case 7:
             this.title = "Leadership";
 						this.description = "Being an inspired, compelling leader of self and others for the benefit of Sogeti and our clients; competently representing Sogeti with pride and confidence that inspires client and co-worker trust  and respect.  Demonstrating the attitude, confidence and professionalism to engage with a client at or one or two levels above your own level. Willingness to assume various roles of leadership throughout the organization.";
-            this.contents[0] = new MainPoint("Main Point 1", ["Sub Point 1.1", "Sub Point 1.2"]);
-            this.contents[1] = new MainPoint("Main Point 2", ["Sub Point 2.1", "Sub Point 2.2", "Sub Point 2.3"]);
+            this.contents[0] = new MainPoint("Training", ["Interns", "Upcoming"]);
+            this.contents[1] = new MainPoint("Client Confidence", ["Interviewing"]);
             break;
 		case 8:
             this.title = "Sogeti Contributions";
@@ -58,15 +57,15 @@ var PageContent = function (pageNumber) {
             break;
 		case 9:
             this.title = "Consultative & Professional Skills";
-						this.description = "Exhibits professionalism and professional behavior at all times, complies with Sogeti and client policies and procedures. Able to make a positive first positive impression, inspiring trust, credibility, and confidence with the client and peers. Has strong professional work ethics. Has solid foundation of IT analytical skills, relevant talents, and productivity abilities.";
+						this.description = "Exhibits professionalism and professional behavior at all times, complies with Sogeti and client policies and procedures. Able to make a positive first impression, inspiring trust, credibility, and confidence with the client and peers. Has strong professional work ethics. Has solid foundation of IT analytical skills, relevant talents, and productivity abilities.";
             this.contents[0] = new MainPoint("Main Point 1", ["Sub Point 1.1", "Sub Point 1.2"]);
             this.contents[1] = new MainPoint("Main Point 2", ["Sub Point 2.1", "Sub Point 2.2", "Sub Point 2.3"]);
             break;
 		case 10:
             this.title = "Practice & Industry Expertise";
 						this.description = "Demonstrating broad understanding of the technology market and excellence in at least one relevant technology, building additional areas of expertise as appropriate.  Taking a strategic vision and tactical interest and involvement in technology innovation to help Sogeti grow.   Active participation and contribution to your Practice or Vertical.";
-            this.contents[0] = new MainPoint("Main Point 1", ["Sub Point 1.1", "Sub Point 1.2"]);
-            this.contents[1] = new MainPoint("Main Point 2", ["Sub Point 2.1", "Sub Point 2.2", "Sub Point 2.3"]);
+            this.contents[0] = new MainPoint("Microsoft Certifications", ["70-515", "70-480", "70-486", "70-487"]);
+            this.contents[1] = new MainPoint("This Presentation...", []);
             break;
 		case 11:
             this.title = "Professional Community";
